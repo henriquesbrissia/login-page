a854ba6061ebc97d61be12578603e1a648678334
Henrique Sbrissia <henriqueerics@gmail.com>
2024-10-14 10:49:38 -0300

    Update App.jsx
    Sets up QueryClient and wrap the App with the QueryClientProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './App.css';
 import { ROUTES } from './utils/routes';
 import { SignIn } from './pages/SignIn';
@@ -6,17 +7,20 @@ import { SignUp } from './pages/SignUp';
 import { ForgotPassword } from './pages/ForgotPassword';
 import { ResetPassword } from './pages/ResetPassword';
 
+const queryClient = new QueryClient();
+
 export const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path={ROUTES.HOME} element={<Navigate to={ROUTES.SIGN_IN} replace />} />
-        <Route path={ROUTES.SIGN_IN} element={<SignIn />} />
-        <Route path={ROUTES.SIGN_UP} element={<SignUp />} />
-        <Route path={ROUTES.FORGOT_PASSWORD} element={<ForgotPassword />} />
-        <Route path={ROUTES.RESET_PASSWORD} element={<ResetPassword />} />
-      </Routes>
-    </Router>
-  )
-}
-
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Routes>
+          <Route path={ROUTES.HOME} element={<Navigate to={ROUTES.SIGN_IN} replace />} />
+          <Route path={ROUTES.SIGN_IN} element={<SignIn />} />
+          <Route path={ROUTES.SIGN_UP} element={<SignUp />} />
+          <Route path={ROUTES.FORGOT_PASSWORD} element={<ForgotPassword />} />
+          <Route path={ROUTES.RESET_PASSWORD} element={<ResetPassword />} />
+        </Routes>
+      </Router>
+    </QueryClientProvider>
+  );
+};
