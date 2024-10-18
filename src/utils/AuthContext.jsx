2d7fcd8dcab48843e72c6b4474de8f99349b6889
Henrique Sbrissia <henriqueerics@gmail.com>
2024-10-18 18:52:18 -0300

    Remove complexity
    Removes unnecessary hooks

--- src/utils/AuthContext.jsx
+++ src/utils/AuthContext.jsx
@@ -1,34 +1,25 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { ROUTES } from "./routes";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [auth, setAuth] = useState(null);
   const navigate = useNavigate();
-
-  useEffect(() => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      setAuth(token);
-    }
-  }, []);
+  const token = localStorage.getItem('token');
 
   const login = (token) => {
     localStorage.setItem('token', token)
-    setAuth(token);
     navigate(ROUTES.DASHBOARD)
   };
 
   const logout = () => {
     localStorage.removeItem('token')
-    setAuth(null)
     navigate(ROUTES.SIGN_IN)
   }
 
   return (
-    <AuthContext.Provider value={{ auth, login, logout }}>
+    <AuthContext.Provider value={{ login, logout, token }}>
       {children}
     </AuthContext.Provider>
   )
